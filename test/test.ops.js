560495201433b93d46c34ec353c702b32b592dbf
Chris <orthagonal1@gmail.com>
2017-08-03 13:29:43 -0500

    add tests for ops logging

--- test/test.ops.js
+++ test/test.ops.js
@@ -1,7 +1,7 @@
 'use strict';
 const tap = require('tap');
 const stats = require('../lib/stats.js');
-
+const poll = require('../lib/poll.js');
 // on some machines the 'free' command returns:
 // total        used        free      shared  buff/cache   available
 tap.test('can parse new-style "free" output', (t) => {
@@ -22,5 +22,41 @@ Mem:       4046848    2571404    1475444       3860     260292     804960
 Swap:            0          0          0              `);
   t.equal(typeof output, 'object');
   t.equal(output.free, 2540696);
+  t.end();
+});
+
+tap.test('logs when cpu stats are exceeded / restored', (t) => {
+  const thresholds = {
+    cpu_one_minute: false,
+    cpu_five_minute: false,
+    cpu_fifteen_minute: false,
+  };
+  const options = {
+    'cpu-one-minute': 0.4,
+    'cpu-five-minute': 0.4,
+    'cpu-fifteen-minute': 0.4,
+  };
+  let results = [];
+  let logr = (tags, msg) => {
+    results.push(msg);
+  };
+  poll.logCPU(options, [0.5, 0.5, 0.5], logr, thresholds);
+  t.notEqual(results[0].indexOf('exceeds'), -1, 'logs one minute cpu average exceeds');
+  t.notEqual(results[1].indexOf('exceeds'), -1, 'logs five minute cpu average exceeds');
+  t.notEqual(results[2].indexOf('exceeds'), -1, 'logs fifteen minute cpu average exceeds');
+  t.equal(thresholds.cpu_one_minute, true, 'tracks that one minute cpu threshold exceeded');
+  t.equal(thresholds.cpu_five_minute, true, 'tracks that five minute cpu threshold exceeded');
+  t.equal(thresholds.cpu_fifteen_minute, true, 'tracks that fifteen minute cpu threshold exceeded');
+  results = [];
+  logr = (tags, msg) => {
+    results.push(msg);
+  };
+  poll.logCPU(options, [0.3, 0.3, 0.3], logr, thresholds);
+  t.notEqual(results[0].indexOf('dropped'), -1, 'logs one minute cpu average restored');
+  t.notEqual(results[1].indexOf('dropped'), -1, 'logs five minute cpu average restored');
+  t.notEqual(results[2].indexOf('dropped'), -1, 'logs fifteen minute cpu average restored');
+  t.equal(thresholds.cpu_one_minute, false, 'tracks that one minute cpu threshold restored');
+  t.equal(thresholds.cpu_five_minute, false, 'tracks that five minute cpu threshold restored');
+  t.equal(thresholds.cpu_fifteen_minute, false, 'tracks that fifteen minute cpu threshold restored');
   t.end();
 });
